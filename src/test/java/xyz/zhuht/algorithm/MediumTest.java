d7ed67e60d9ab37ad5e2d0a1a5ebdae3a3557f73
zhuht <mnizhuht@gmail.com>
2020-05-25 18:35:16 +0800

    add 146 LRU

---
+++ src/test/java/xyz/zhuht/algorithm/MediumTest.java
@@ -0,0 +1,22 @@
+package xyz.zhuht.algorithm;
+
+import org.junit.Test;
+import xyz.zhuht.algorithm.medium.LRUHuanCunJiZhi;
+
+/**
+ * zhuhaitao
+ * 2020/5/25 10:48
+ * 算法-中级 测试
+ */
+
+public class MediumTest {
+
+  /**
+   * 146.LRU缓存机制
+   */
+  @Test
+  public void LRUHuanCunJiZhiTest() {
+    LRUHuanCunJiZhi lru = new LRUHuanCunJiZhi();
+    System.out.println("a");
+  }
+}
