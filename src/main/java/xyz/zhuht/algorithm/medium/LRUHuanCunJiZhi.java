95bf45c96272d6513441ced4d0967fb499c6bdcf
zhuht <mnizht@qq.com>
2020-05-25 21:21:32 +0800

    完善146 LRU cache

--- src/main/java/xyz/zhuht/algorithm/medium/LRUHuanCunJiZhi.java
+++ src/main/java/xyz/zhuht/algorithm/medium/LRUHuanCunJiZhi.java
@@ -72,30 +72,51 @@ public class LRUHuanCunJiZhi {
     tail.prev = head;
   }
 
-  private int get(int key){
+  public int get(int key) {
     DLinkedNode node = cache.get(key);
-    if(null == node){
+    if (null == node) {
       return -1;
     }
     moveToHead(node);
     return node.value;
   }
 
-  private void addToHead(DLinkedNode node){
+  public void put(int key, int value) {
+    DLinkedNode node = cache.get(key);
+    if (null == node) {
+      DLinkedNode newNode = new DLinkedNode(key, value);
+      cache.put(key, newNode);
+      addToHead(newNode);
+      ++size;
+      if (size > capacity) {
+        DLinkedNode tail = removeTail();
+        cache.remove(tail.key);
+        --size;
+      }
+    } else {
+      node.value = value;
+      moveToHead(node);
+    }
+  }
+
+  private void addToHead(DLinkedNode node) {
     node.prev = head;
     node.next = head.next;
     head.next.prev = node;
     head.next = node;
   }
-  private void removeNode(DLinkedNode node){
+
+  private void removeNode(DLinkedNode node) {
     node.prev.next = node.next;
     node.next.prev = node.prev;
   }
-  private void moveToHead(DLinkedNode node){
+
+  private void moveToHead(DLinkedNode node) {
     removeNode(node);
     addToHead(node);
   }
-  private DLinkedNode removeTail(){
+
+  private DLinkedNode removeTail() {
     DLinkedNode res = tail.prev;
     removeNode(res);
     return res;
