0fafd7d3fe1e6e1ead879d1345d49fa816137230
zhuht <mnizhuht@gmail.com>
2020-06-09 19:02:27 +0800

    没看懂代码。。没用官方的解法，在评论区找到个递归解法。

---
+++ src/main/java/xyz/zhuht/algorithm/medium/BaShuZiFanYiChengZiFuChuan.java
@@ -0,0 +1,41 @@
+package xyz.zhuht.algorithm.medium;
+
+/**
+ * zhuhaitao
+ * 2020/6/9 14:12
+ * 面试题46. 把数字翻译成字符串
+ * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 12258
+ * 输出: 5
+ * 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= num < 2^31
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class BaShuZiFanYiChengZiFuChuan {
+  public int translateNum(int num) {
+    // num为个位数时，只有一种情况
+    if (num < 10) {
+      return 1;
+    }
+    // 取num的后两位
+    int endTwo = num % 100;
+    if (endTwo < 26 && endTwo > 9) {
+      return translateNum(num / 10) + translateNum(num / 100);
+    } else {
+      // num 后两位数字小于10或大于25时，
+      return translateNum(num / 10);
+    }
+  }
+}
