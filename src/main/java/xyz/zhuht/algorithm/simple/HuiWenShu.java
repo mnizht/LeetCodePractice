32653b2f24f13b3d9358632220c94d0b1943c3e7
zhuht <mnizhuht@gmail.com>
2020-06-10 12:56:10 +0800

    回文数，折半比较，逻辑和代码都能看懂了，，，确实是简单题

---
+++ src/main/java/xyz/zhuht/algorithm/simple/HuiWenShu.java
@@ -0,0 +1,45 @@
+package xyz.zhuht.algorithm.simple;
+
+/**
+ * zhuhaitao
+ * 2020/6/10 12:46
+ * 9.回文数
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 121
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3:
+ * <p>
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ * 进阶:
+ * <p>
+ * 你能不将整数转为字符串来解决这个问题吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class HuiWenShu {
+  public boolean isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+      return false;
+    }
+
+    int revertedNum = 0;
+    while (revertedNum < x) {
+      revertedNum = revertedNum * 10 + x % 10;
+      x = x / 10;
+    }
+
+    return revertedNum == x || revertedNum / 10 == x;
+  }
+}
