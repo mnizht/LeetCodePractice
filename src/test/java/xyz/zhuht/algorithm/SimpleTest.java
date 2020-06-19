63c31f5ebc524d0bdb539df6243d73e40529d1f3
zhuht <mnizht@qq.com>
2020-06-19 23:07:11 +0800

    简单题，思路写出来了，api用的不好，继续cv

---
+++ src/test/java/xyz/zhuht/algorithm/SimpleTest.java
@@ -0,0 +1,30 @@
+package xyz.zhuht.algorithm;
+
+import org.junit.Test;
+import xyz.zhuht.algorithm.simple.YanZhengHuiWenChuan;
+
+/**
+ * @author haitao zhu
+ * @date 2020/6/19 23:00
+ */
+public class SimpleTest {
+
+  @Test
+  public void YanZhengHuiWenChuanTest(){
+    YanZhengHuiWenChuan test = new YanZhengHuiWenChuan();
+    String str1 = "A man, a plan, a canal: Panama";
+    String str2 = "race a car";
+    String str3 = "";
+    String str4 = " ";
+    String str5 = ",";
+    String str6 = "a";
+
+    System.out.println(test.isPalindrome(str1));
+    System.out.println(test.isPalindrome(str2));
+    System.out.println(test.isPalindrome(str3));
+    System.out.println(test.isPalindrome(str4));
+    System.out.println(test.isPalindrome(str5));
+    System.out.println(test.isPalindrome(str6));
+
+  }
+}
