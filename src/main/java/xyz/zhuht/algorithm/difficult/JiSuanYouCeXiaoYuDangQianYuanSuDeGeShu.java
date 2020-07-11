20eb6a8452177b6787b796f08636dc08c3590ace
zhuht <mnizht@qq.com>
2020-07-11 09:18:52 +0800

    看了题解。。看了评论，这道题好像确实挺难，题解也没看懂

--- src/main/java/xyz/zhuht/algorithm/difficult/JiSuanYouCeXiaoYuDangQianYuanSuDeGeShu.java
+++ src/main/java/xyz/zhuht/algorithm/difficult/JiSuanYouCeXiaoYuDangQianYuanSuDeGeShu.java
@@ -1,7 +1,11 @@
 package xyz.zhuht.algorithm.difficult;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author haitao zhu
@@ -24,17 +28,63 @@ import java.util.List;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class JiSuanYouCeXiaoYuDangQianYuanSuDeGeShu {
+  private int[] c;
+  private int[] a;
+
   public List<Integer> countSmaller(int[] nums) {
-    List<Integer> ans = new ArrayList<>();
-    for (int i = 0; i < nums.length; i++) {
-      int count = 0;
-      for (int j = i; j < nums.length; j++) {
-        if (nums[j] < nums[i]) {
-          count++;
-        }
-      }
-      ans.add(count);
+    List<Integer> resultList = new ArrayList<Integer>();
+    discretization(nums);
+    init(nums.length + 5);
+    for (int i = nums.length - 1; i >= 0; --i) {
+      int id = getId(nums[i]);
+      resultList.add(query(id - 1));
+      update(id);
     }
-    return ans;
+    Collections.reverse(resultList);
+    return resultList;
+  }
+
+  private void init(int length) {
+    c = new int[length];
+    Arrays.fill(c, 0);
+  }
+
+  private int lowBit(int x) {
+    return x & (-x);
+  }
+
+  private void update(int pos) {
+    while (pos < c.length) {
+      c[pos] += 1;
+      pos += lowBit(pos);
+    }
+  }
+
+  private int query(int pos) {
+    int ret = 0;
+    while (pos > 0) {
+      ret += c[pos];
+      pos -= lowBit(pos);
+    }
+
+    return ret;
+  }
+
+  private void discretization(int[] nums) {
+    Set<Integer> set = new HashSet<Integer>();
+    for (int num : nums) {
+      set.add(num);
+    }
+    int size = set.size();
+    a = new int[size];
+    int index = 0;
+    for (int num : set) {
+      a[index++] = num;
+    }
+    Arrays.sort(a);
+  }
+
+  private int getId(int x) {
+    return Arrays.binarySearch(a, x) + 1;
   }
 }
