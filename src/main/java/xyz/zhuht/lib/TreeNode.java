fd039334eb9c568c4c34eb7fdc325b5f163ff745
zhuht <mnizht@qq.com>
2020-08-07 07:28:02 +0800

    真是简单题，自己写的递归，和官方题解逻辑一样。。。我是不是原来做过这道题

--- src/main/java/xyz/zhuht/lib/TreeNode.java
+++ src/main/java/xyz/zhuht/lib/TreeNode.java
@@ -9,6 +9,9 @@ public class TreeNode {
   public TreeNode left;
   public TreeNode right;
 
+  public TreeNode() {
+  }
+
   public TreeNode(int val, TreeNode left, TreeNode right) {
     this.val = val;
     this.left = left;
